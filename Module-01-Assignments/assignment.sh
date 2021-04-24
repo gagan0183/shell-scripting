07683b7b2d785949cbfefa926a2098050edbb16c
Gagan <g.deepsingh1@gmail.com>
2021-04-24 18:17:44 +0530

    assignment 1

---
+++ Module-01-Assignments/assignment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+printf "%5s %15s %10s %16s %16s\n" "ID" "COURSE NAME" "LEVEL" "ACTUAL PRICE" "DISC. PRICE"
+echo "-------------------------------------------------------------------------------"
+
+printf "%5d %15s %10s %16.2f %16.2f\n" 10 "Bash" "L100" 99 12
+printf "%5d %15s %10s %16.2f %16.2f\n" 105 "Linux" "L200" 1599 15
